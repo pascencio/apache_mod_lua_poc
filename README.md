6d6bd9382e20e328d52516af195a639413c7d4a5
Patricio Ascencio <patricio.ascencio@karibu.cl>
2020-02-06 17:40:35 -0300

    Documentation

---
+++ README.md
@@ -0,0 +1,29 @@
+# Proof of concept for apache's HTTP Lua module
+
+- [Proof of concept for apache's HTTP Lua module](#proof-of-concept-for-apaches-http-lua-module)
+  - [Requirements](#requirements)
+  - [Running the POC](#running-the-poc)
+
+This POC trying to proof that the apache's HTTP Lua module can be used in a Reverse Proxy to control the access to internal resources. 
+
+## Requirements
+
+- Docker versión 19 (Required)
+- docker-compose versión 1.22 (Required)
+- Linux (Optional)
+
+## Running the POC
+
+To start the sample application you must execute the following sentence (For Linux).
+
+```shell
+docker-compose up -d && docker-compose logs -f
+```
+
+When the logs stop to writing to standard output. You will have access to the REST API used to proof the access control.
+
+You can use the following [Postman collection](doc/API_Dummy.postman_collection.json) to test the API.
+
+With the collection imported, you can test the API such as shown in the gif:
+
+![Postam example](doc/postman_example.gif)
--- api/server.js
+++ api/server.js
@@ -2,9 +2,11 @@ const fastify = require('fastify')({ logger: true });
 
 fastify.get('/customer/:id', async (request, reply) => {
     let services = [{
-        "name": "Medidor modelo X123RE",
+        "name": "Air conditioning",
     }, {
-        "name": "Medidor inteligente modelo ERTPX0",
+        "name": "Solar panel",
+    }, {
+        "name": "SMS consumption alerts",
     }];
     return {
         "customer_id": request.params.id,
@@ -17,15 +19,15 @@ fastify.get('/error/:code', async (request, reply) => {
 
     switch (request.params.code) {
         case "403":
-            message = "No tiene acceso al recurso solicitado";
+            message = "You don't have access to requested resource";
             reply.code(403);
             break;
         case "404":
-            message = "El recurso solicitado no existe";
+            message = "Requested resource not exists";
             reply.code(404);
             break
         default:
-            message = "Error interno, intentelo más tarde";
+            message = "Internal error, try again later";
             reply.code(500);
             break
     }
@@ -48,7 +50,7 @@ fastify.post('/auth', async (request, reply) => {
 const start = async () => {
     try {
         await fastify.listen(3000, '0.0.0.0')
-        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+        fastify.log.info(`Server listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
